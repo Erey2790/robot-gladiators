f807a9aef1b9d9addeea65e898d3590d4c9cb308
ElmerPC <erey2790@gmail.com>
2021-11-30 21:59:39 -0600

    fight or FIGHT code

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,28 +14,41 @@ var fight = function() {
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining." 
     )
-  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    playerHealth = playerHealth - enemyAttack;
-  // Log a resulting message to the console so we know that it worked.
-    console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    )
 
-    // check player's health
-    if (playerHealth <= 0) {
-      window.alert(playerName + " has died!");
-    }
-    else {
-      window.alert(playerName + " still has " + playerHealth + " health left.");
-    }
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'skip' to choose.")
 
-    // check enemy's health
-    if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
-    }
-    else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
-    }
-};
+
+// if player choses to fight, then fight
+if (promptFight === "fight" || promptFight === "FIGHT") {
+  // remove enemy's health by subtracting the amount set in the playerAttack variable
+  enemyHealth = enemyHealth - playerAttack;
+  console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  );
+}
+
+//   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+//     playerHealth = playerHealth - enemyAttack;
+//   // Log a resulting message to the console so we know that it worked.
+//     console.log(
+//         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+//     )
+
+//     // check player's health
+//     if (playerHealth <= 0) {
+//       window.alert(playerName + " has died!");
+//     }
+//     else {
+//       window.alert(playerName + " still has " + playerHealth + " health left.");
+//     }
+
+//     // check enemy's health
+//     if (enemyHealth <= 0) {
+//       window.alert(enemyName + " has died!");
+//     }
+//     else {
+//       window.alert(enemyName + " still has " + enemyHealth + " health left.");
+//     }
+// };
 
 fight();
